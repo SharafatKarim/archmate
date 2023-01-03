015c97d3b792ddc25b70e53816ecd91ad41df825
Sharafat <93897936+SharafatKarim@users.noreply.github.com>
2023-01-03 21:36:14 +0600

    uninstaller script - flag removed

--- amate.py
+++ amate.py
@@ -153,7 +153,6 @@ all_commands = [
     Command("Information Center", "DMI table", "sudo dmidecode", ""),
     Command("Information Center", "IP address", "ip addr", ""),
 
-    #TODO: uninstall script
     Command("Troubleshooting", "Uninstall this script (if installed)", "curl -s https://raw.githubusercontent.com/SharafatKarim/archmate/main/uninstall.py > .amate-uninstaller.py && python .amate-uninstaller.py && rm .amate-uninstaller.py", ""),
     Command("Troubleshooting", "Manjaro Bangla font issue", "curl -s https://raw.githubusercontent.com/SharafatKarim/Manjaro-Bangla-Font-Fix/main/main.sh | bash", ""),
 ]
